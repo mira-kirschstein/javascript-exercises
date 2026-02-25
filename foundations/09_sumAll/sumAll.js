8d3ef4eceb4db0a1cacd1fef1d71f1bf5c94cb8d
Mira <mirakirschstein@gmail.com>
2026-02-25 14:15:31 +0100

    task 09 forgot to save earlier

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,3 +1,5 @@
+// My solution
+
 const sumAll = function(int1, int2) {
     //Check if ints are positiv
     let sum = 0;
@@ -23,3 +25,23 @@ const sumAll = function(int1, int2) {
 
 // Do not edit below this line
 module.exports = sumAll;
+
+//ToP Solution
+/* Difference = instead of the same code for adding twice, they swap min and max when min > max
+const sumAll = function(min, max){
+    if(!Number.isInteger(min) || !Number.isInteger(max)) return "ERROR";
+    if(min<0 || max<0) return "ERROR";
+    if(min > max) {
+        const temp = min;
+        min = max;
+        max = temp;
+    }
+
+    let sum = 0;
+    for (let i=min; i <= max; i++){
+        sum+= i;
+    }
+    return sum;
+};
+
+*/
