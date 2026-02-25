bf613961f6e4c22599326ec0f9677de0e820b714
Mira <mirakirschstein@gmail.com>
2026-02-25 14:14:32 +0100

    finished task 09

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function(int1, int2) {
+    //Check if ints are positiv
+    let sum = 0;
 
+    //check if both ints are positive 
+    if( int1 > 0 && int2 > 0 && Number.isInteger(int1) && Number.isInteger(int2)){
+
+        //swap ints so order of input doesnt matter
+        if(int1>int2){
+            for(let i=int2; i<= int1; i++){
+                sum += i;
+            }
+        } else{
+            for(let i = int1; i<=int2; i++){
+              sum += i;
+            }
+        }
+    } else {
+        return "ERROR";
+    }
+    return sum;
 };
 
 // Do not edit below this line
