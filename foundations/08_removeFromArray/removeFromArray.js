f4b70e447e0b109316534a26e82ea385b9eab3a9
Mira <mirakirschstein@gmail.com>
2026-02-25 13:44:48 +0100

    task 08

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    // create a new empty array
+    const newArray = [];
+    
+    //use forEach to fo through the array
+    array.forEach((item) => {
+        if (!args.includes(item)) {
+            // push every element into the new array
+            // UNLESS it is included in the function arguments
+            newArray.push(item);
+        }   
+    });
+    return newArray;
 };
 
 // Do not edit below this line
